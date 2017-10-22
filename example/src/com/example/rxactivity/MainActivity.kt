ae4355525053943a5482ad7c970bbe899b6bba81
Hendra Anggrian <hendraanggrian@gmail.com>
2017-10-23 05:06:10 +0700

    partially fixes issue #2

--- example/src/com/example/rxactivity/MainActivity.kt
+++ example/src/com/example/rxactivity/MainActivity.kt
@@ -6,15 +6,18 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.preference.ListPreference
 import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
+import android.view.View
 import com.hendraanggrian.app.RxActivity
 import com.hendraanggrian.app.startActivityForResultAsCompletable
 import com.hendraanggrian.app.startActivityForResultAsObservable
 import com.hendraanggrian.app.startActivityForResultAsSingle
 import io.reactivex.rxkotlin.subscribeBy
 import kota.*
+import kota.dialogs.OkButton
+import kota.dialogs.supportAlert
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,63 +31,65 @@ class MainActivity : AppCompatActivity() {
         RxActivity.onActivityResult(requestCode, resultCode, data)
     }
 
+    override fun onClick(v: View?) {
+        when (sharedPreferences.getString("typePreference")) {
+            "Observable" -> startActivityForResultAsObservable(Intent(this, NextActivity::class.java))
+                    .subscribeBy(
+                            onNext = { _ -> onResultOK() },
+                            onError = { e -> onResultError(e) },
+                            onComplete = { onResultComplete() })
+            "Single" -> startActivityForResultAsSingle(Intent(this, NextActivity::class.java))
+                    .subscribeBy(
+                            onSuccess = { _ -> onResultOK() },
+                            onError = { e -> onResultError(e) })
+            "Completable" -> startActivityForResultAsCompletable(Intent(this, NextActivity::class.java))
+                    .subscribeBy(
+                            onComplete = { onResultComplete() },
+                            onError = { e -> onResultError(e) })
+        }
+    }
+
+    private fun onResultOK() = supportAlert("Result", "onNext", OkButton)
+    private fun onResultError(e: Throwable) = supportAlert("Result", "onError: ${e.message}", OkButton)
+    private fun onResultComplete() = toast("onComplete")
+
     class Content : PreferenceFragmentCompat() {
         private lateinit var typePreference: ListPreference
-        private lateinit var activityPreference: Preference
         private lateinit var fragmentPreference: Preference
 
-        private var type = "Observable"
-        private val isObservable get() = type == "Observable"
-        private val isSingle get() = type == "Single"
-        private val isCompletable get() = type == "Completable"
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            retainInstance = true
+        }
 
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             addPreferencesFromResource(R.xml.activity_main)
             typePreference = find("typePreference")
-            activityPreference = find("activityPreference")
             fragmentPreference = find("fragmentPreference")
 
+            typePreference.value?.let { typePreference.summary = "As $it" }
             typePreference.setOnPreferenceChangeListener { preference, newValue ->
-                newValue as String
                 preference.summary = "As $newValue"
-                type = newValue
-                false
-            }
-            activityPreference.setOnPreferenceClickListener {
-                when {
-                    isObservable -> activity.startActivityForResultAsObservable(Intent(context, NextActivity::class.java))
-                            .subscribeBy(
-                                    onNext = { _ -> (activity as MainActivity).onResultOK() },
-                                    onError = { e -> (activity as MainActivity).onResultError(e) },
-                                    onComplete = { (activity as MainActivity).onResultComplete() })
-                    isSingle -> activity.startActivityForResultAsSingle(Intent(context, NextActivity::class.java))
-                            .subscribeBy(
-                                    onSuccess = { _ -> (activity as MainActivity).onResultOK() },
-                                    onError = { e -> (activity as MainActivity).onResultError(e) })
-                    isCompletable -> activity.startActivityForResultAsCompletable(Intent(context, NextActivity::class.java))
-                            .subscribeBy(
-                                    onComplete = { (activity as MainActivity).onResultComplete() },
-                                    onError = { e -> (activity as MainActivity).onResultError(e) })
-                }
                 true
             }
+
             fragmentPreference.setOnPreferenceClickListener {
-                when {
-                    isObservable -> startActivityForResultAsObservable(Intent(context, NextActivity::class.java))
+                when (typePreference.value) {
+                    "Observable" -> startActivityForResultAsObservable(Intent(context, NextActivity::class.java))
                             .subscribeBy(
                                     onNext = { _ -> onResultOK() },
                                     onError = { e -> onResultError(e) },
                                     onComplete = { onResultComplete() })
-                    isSingle -> startActivityForResultAsSingle(Intent(context, NextActivity::class.java))
+                    "Single" -> startActivityForResultAsSingle(Intent(context, NextActivity::class.java))
                             .subscribeBy(
                                     onSuccess = { _ -> onResultOK() },
                                     onError = { e -> onResultError(e) })
-                    isCompletable -> startActivityForResultAsCompletable(Intent(context, NextActivity::class.java))
+                    "Completable" -> startActivityForResultAsCompletable(Intent(context, NextActivity::class.java))
                             .subscribeBy(
                                     onComplete = { onResultComplete() },
                                     onError = { e -> onResultError(e) })
                 }
-                true
+                false
             }
         }
 
@@ -93,12 +98,8 @@ class MainActivity : AppCompatActivity() {
             RxActivity.onActivityResult(requestCode, resultCode, data)
         }
 
-        private fun onResultOK() = activity.contentView!!.snackbar("onNext", android.R.string.ok) {}
-        private fun onResultError(e: Throwable) = activity.contentView!!.snackbar("onError: ${e.message}", android.R.string.ok) {}
+        private fun onResultOK() = supportAlert("Result", "onNext", OkButton)
+        private fun onResultError(e: Throwable) = supportAlert("Result", "onError: ${e.message}", OkButton)
         private fun onResultComplete() = context.toast("onComplete")
     }
-
-    private fun onResultOK() = contentView!!.snackbar("onNext", android.R.string.ok) {}
-    private fun onResultError(e: Throwable) = contentView!!.snackbar("onError: ${e.message}", android.R.string.ok) {}
-    private fun onResultComplete() = toast("onComplete")
 }
--- rx-activity/src/com/hendraanggrian/app/RxActivity.kt
+++ rx-activity/src/com/hendraanggrian/app/RxActivity.kt
@@ -1,7 +1,6 @@
 package com.hendraanggrian.app
 
 import android.content.Intent
-import android.support.v4.app.FragmentActivity
 import android.support.v4.util.SparseArrayCompat
 import java.lang.ref.WeakReference
 import java.util.*
