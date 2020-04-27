9ff01ea45080e7f2d399f50afac636e9ab42e5ad
Hendra Anggrian <hendraanggrian@gmail.com>
2020-04-27 23:47:47 +0700

    Simpler preference fragment

--- example/src/com/example/launchy/MainActivity.kt
+++ example/src/com/example/launchy/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.example.launchy
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.bannerbar
 import com.hendraanggrian.appcompat.launchy.Launchy
-import com.hendraanggrian.appcompat.launchy.launchPermission
 import com.hendraanggrian.prefy.Prefy
-import com.hendraanggrian.prefy.android.AndroidPreferences
 import com.hendraanggrian.prefy.android.get
 import com.jakewharton.processphoenix.ProcessPhoenix
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
-    private lateinit var preferences: AndroidPreferences
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,17 +20,6 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
             .beginTransaction()
             .replace(R.id.container, MainFragment())
             .commitNow()
-        preferences = Prefy[this]
-    }
-
-    override fun onResume() {
-        super.onResume()
-        preferences.registerOnSharedPreferenceChangeListener(this)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        preferences.unregisterOnSharedPreferenceChangeListener(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -46,19 +30,11 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.resetItem -> {
-                preferences.edit { clear() }
+                Prefy[this].edit { clear() }
                 ProcessPhoenix.triggerRebirth(this)
             }
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    override fun onSharedPreferenceChanged(p: SharedPreferences, key: String) {
-        launchPermission(*preferences.getStringSet(key, emptySet())!!.toTypedArray()) { isGranted ->
-            container.bannerbar("Permission result") {
-                subtitle = if (isGranted) "All permissions are granted." else "At least one permission is denied."
-            }
-        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
--- example/src/com/example/launchy/MainFragment.kt
+++ example/src/com/example/launchy/MainFragment.kt
@@ -1,10 +1,18 @@
 package com.example.launchy
 
+import android.Manifest
+import android.app.Dialog
 import android.os.Bundle
-import androidx.annotation.ArrayRes
-import androidx.preference.MultiSelectListPreference
+import android.util.Log
+import android.widget.FrameLayout
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.core.content.PermissionChecker
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.snackbar.bannerbar
+import com.hendraanggrian.appcompat.launchy.Launchy
+import com.hendraanggrian.appcompat.launchy.launchPermission
 
 class MainFragment : PreferenceFragmentCompat() {
 
@@ -13,34 +21,62 @@ class MainFragment : PreferenceFragmentCompat() {
         findPreference<Preference>("launchActivity")!!.setOnPreferenceClickListener {
             false
         }
-        findPreference<MultiSelectListPreference>("launchPermissions")!!.bindSummary({ values }) {
-            it.joinToString(", ") { getActualString(it, R.array.permission_values, R.array.permissions) }
+        findPreference<Preference>("launchPermissions")!!.setOnPreferenceClickListener {
+            PermissionsDialog().show(childFragmentManager, null)
+            false
         }
     }
 
-    /**
-     * @param initial starting value can be obtained from its value, text, etc.
-     * @param convert its preference value to representable summary text.
-     */
-    private fun <P : Preference, T> P.bindSummary(
-        initial: P.() -> T?,
-        convert: (T) -> CharSequence? = { it?.toString() }
-    ) {
-        initial()?.let { summary = convert(it) }
-        onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
-            @Suppress("UNCHECKED_CAST")
-            preference.summary = convert(newValue as T)
-            true
-        }
-    }
+    class PermissionsDialog : AppCompatDialogFragment() {
 
-    private fun getActualString(
-        s: CharSequence,
-        @ArrayRes arrayValuesRes: Int,
-        @ArrayRes arraysRes: Int
-    ): CharSequence {
-        val arrayValues = resources.getStringArray(arrayValuesRes)
-        val arrays = resources.getStringArray(arraysRes)
-        return arrays[arrayValues.indexOf(s)]
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            val requestedPermissions = mutableListOf<String>()
+            return AlertDialog.Builder(context!!)
+                .setMultiChoiceItems(
+                    arrayOf("Calendar", "Camera", "Contacts", "SMS"),
+                    booleanArrayOf(false, false, false, false)
+                ) { _, which, isChecked ->
+                    if (isChecked) {
+                        when (which) {
+                            0 -> requestedPermissions += Manifest.permission.READ_CALENDAR
+                            1 -> requestedPermissions += Manifest.permission.CAMERA
+                            2 -> requestedPermissions += Manifest.permission.READ_CONTACTS
+                            3 -> requestedPermissions += Manifest.permission.READ_SMS
+                        }
+                    } else {
+                        when (which) {
+                            0 -> requestedPermissions -= Manifest.permission.READ_CALENDAR
+                            1 -> requestedPermissions -= Manifest.permission.CAMERA
+                            2 -> requestedPermissions -= Manifest.permission.READ_CONTACTS
+                            3 -> requestedPermissions -= Manifest.permission.READ_SMS
+                        }
+                    }
+                }
+                .setNegativeButton(android.R.string.cancel) { _, _ -> }
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    launchPermission(*requestedPermissions.toTypedArray()) { isGranted ->
+                        dialog!!.window!!.findViewById<FrameLayout>(android.R.id.content).bannerbar(
+                            when {
+                                isGranted -> "All permissions are granted"
+                                else -> "At least one permission is denied"
+                            }
+                        ) {
+                            subtitle = buildString {
+                                val getResult: (permission: String) -> String = {
+                                    when (PermissionChecker.PERMISSION_GRANTED) {
+                                        PermissionChecker.checkSelfPermission(context!!, it) -> "granted."
+                                        else -> " denied."
+                                    }
+                                }
+                                appendln("Calendar is ${getResult(Manifest.permission.READ_CALENDAR)}")
+                                appendln("Camera is ${getResult(Manifest.permission.CAMERA)}")
+                                appendln("Contacts is ${getResult(Manifest.permission.READ_CONTACTS)}")
+                                append("SMS is ${getResult(Manifest.permission.READ_SMS)}")
+                            }
+                        }
+                    }
+                }
+                .create()
+        }
     }
 }
